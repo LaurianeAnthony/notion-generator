6235039b1e55357cb9b55af7295bb58d44aa42df
Lauriane Anthony <lauriane.anthony@gmail.com>
2020-04-05 18:30:52 +0200

    Updates

---
+++ precache-manifest.ef1d4b838aab5bb5263b4714107a3256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05d8158cdba34cfa0e901bb65851f89f",
+    "url": "/notion-generator/index.html"
+  },
+  {
+    "revision": "9745f74bb259eb6e519f",
+    "url": "/notion-generator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "04aa03ff818583af9545",
+    "url": "/notion-generator/static/js/2.de514a25.chunk.js"
+  },
+  {
+    "revision": "33fafc315076a65abc9121eb3cf638e7",
+    "url": "/notion-generator/static/js/2.de514a25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9745f74bb259eb6e519f",
+    "url": "/notion-generator/static/js/main.b8fceeb1.chunk.js"
+  },
+  {
+    "revision": "13735ed1af5b919bbb8a",
+    "url": "/notion-generator/static/js/runtime-main.206ac6c4.js"
+  }
+]);
